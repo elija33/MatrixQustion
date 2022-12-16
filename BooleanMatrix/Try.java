70ab53467e41a449d4572be16978e2175bdfe58e
Elija33 <amponsaeli@gmail.com>
2022-12-15 23:02:09 -0500

    Boolean Matric

---
+++ BooleanMatrix/Try.java
@@ -0,0 +1,12 @@
+package BooleanMatrix;
+
+/*
+Given a list of records as shown above, return a collection of all the people that appear in the records. 
+
+Note: each person should only appear once in the output.
+
+Expected output: ["Paul", "Curtis", "Steve", "Martha", "Jennifer"] */
+
+public class Try {
+
+}
